9a19cdfe91638dba0ceb562f80fbdd3199cf6b22
Armando Linares <armandolinaresg@gmail.com>
2024-08-03 23:38:37 -0600

    Lower layer weird position fix

--- crkbd/keymaps/ei/keymap.c
+++ crkbd/keymaps/ei/keymap.c
@@ -19,43 +19,43 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
     KC_LCTL, KC_Z,   KC_X,    KC_C,    KC_D,    KC_V,                          KC_K, KC_H,TD(TD_MEN),TD(TD_MAY),TD(TD_SLA),KC_LALT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                        TD(TD_CAPLOCK),MO(1),KC_SPC,        KC_ENT, MO(2), KC_LGUI
+                                        MO(1),TD(TD_CAPLOCK),KC_SPC,        KC_ENT, MO(2), KC_LGUI
                                       //`--------------------------'  `--------------------------'
   ),
 
   [1] = LAYOUT_split_3x6_3 (
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR,                      KC_EQL,  KC_7, KC_8, KC_9, KC_0,    XXXXXXX,
+    KC_F7,  KC_F8,  KC_F9,  KC_F10,  XXXXXXX,  XXXXXXX,                        KC_EQL,    KC_7,   KC_8,    KC_9,   KC_0,     XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LPRN, KC_RPRN,                      KC_MINS, KC_4, KC_5, KC_6, KC_PLUS, KC_GRV,
+    KC_F4,  KC_F5,  KC_F6,  KC_F11,  XXXXXXX,  XXXXXXX,                        KC_MINS,   KC_4,   KC_5,    KC_6,   KC_PLUS,  XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC,                      KC_UNDS, KC_1, KC_2, KC_3, KC_BSLS, XXXXXXX,
+    KC_F1,  KC_F2,  KC_F3,  KC_F12,  XXXXXXX,  XXXXXXX,                        KC_UNDS,   KC_1,   KC_2,    KC_3,   KC_ASTR,  XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                        XXXXXXX, _______, XXXXXXX,         XXXXXXX, MO(3), XXXXXXX
+                                        _______, XXXXXXX, XXXXXXX,         XXXXXXX, MO(3), XXXXXXX
                                       //`--------------------------'  `--------------------------'
   ),
 
   [2] = LAYOUT_split_3x6_3 (
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-    XXXXXXX, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_PIPE, KC_TILD,  KC_DEL,
+    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                      KC_CIRC, KC_AMPR, KC_PIPE, XXXXXXX, XXXXXXX,  KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LPRN, KC_RPRN,                      XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_GRV,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR,                      XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX, KC_BSLS,  XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                        XXXXXXX, MO(3), XXXXXXX,         XXXXXXX, _______, XXXXXXX
+                                        MO(3), XXXXXXX, XXXXXXX,         XXXXXXX, _______, XXXXXXX
                                       //`--------------------------'  `--------------------------'
   ),
 
   [3] = LAYOUT_split_3x6_3 (
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-    QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MUTE,                      XXXXXXX, KC_F7, KC_F8, KC_F9, KC_F10, KC_PSCR,
+    QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-    RGB_TOG, RGB_HUI, RGB_VAI, RGB_SPI, XXXXXXX, KC_VOLU,                      XXXXXXX, KC_F4, KC_F5, KC_F6, KC_F11, XXXXXXX,
+    RGB_TOG, RGB_HUI, RGB_VAI, RGB_SPI, XXXXXXX, KC_VOLU,                      KC_MUTE, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-    RGB_MOD, RGB_HUD, RGB_VAD, RGB_SPD, XXXXXXX, KC_VOLD,                      XXXXXXX, KC_F1, KC_F2, KC_F3, KC_F12, XXXXXXX,
+    RGB_MOD, RGB_HUD, RGB_VAD, RGB_SPD, XXXXXXX, KC_VOLD,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                        XXXXXXX, _______, XXXXXXX,       XXXXXXX, _______, XXXXXXX
+                                        _______, XXXXXXX, XXXXXXX,       XXXXXXX, _______, XXXXXXX
                                       //`--------------------------'  `--------------------------'
   )
 };
