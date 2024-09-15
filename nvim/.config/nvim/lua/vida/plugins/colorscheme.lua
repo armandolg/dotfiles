b47f3ac14cdd2eb0495c9ba0d4bcd6fb2b3c15cf
Armando Linares <armandolinaresg@gmail.com>
2024-09-14 21:46:38 -0600

    Gruvbox era?

--- nvim/.config/nvim/lua/vida/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/vida/plugins/colorscheme.lua
@@ -1,8 +1,12 @@
 return {
-	"bluz71/vim-moonfly-colors",
+	-- "bluz71/vim-moonfly-colors",
+	"sainnhe/gruvbox-material",
 	priority = 1000,
 
 	config = function()
-		vim.cmd("colorscheme moonfly")
+		-- vim.cmd("colorscheme moonfly")
+		vim.g.gruvbox_material_background = "hard"
+		vim.g.gruvbox_material_better_performance = 1
+		vim.cmd("colorscheme gruvbox-material")
 	end,
 }
