149e12864ca497943fb92402afc73e768275e664
Armando Linares <armandolinaresg@gmail.com>
2024-09-21 16:08:15 -0600

    Hotfix indentation

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -50,6 +50,9 @@ vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 15
+
+-- Obsidian
+vim.opt.conceallevel = 1
 
 -- vim: ts=2 sts=2 sw=2 et
